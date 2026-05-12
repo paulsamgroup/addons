c917499c098245f4e5432729d7199139bf2a41d8
dericking01 <dericking01@gmail.com>
2026-05-13 00:13:54 +0400

    hide Print RFQ Button

---
+++ hide_print_rfq_button/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Hide Print RFQ Button',
+    'version': '18.0.1.0.0',
+    'summary': 'Hide Print RFQ button on purchase order in all states',
+    'category': 'Purchase',
+    'author': 'Primesoft Technologies',
+    'license': 'LGPL-3',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
